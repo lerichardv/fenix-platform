b2172f52fbbcd0b81195f62b6b489ddfd573cee6
Ricardo Valladares <lerichard@Ricardos-MacBook-Pro.local>
2026-09-18 14:49:41 -0600

    subiendo tema de variable payroll app url

--- redirect.php
+++ redirect.php
@@ -8,16 +8,25 @@
  * @author lerichard <ric.valladares@bluelionsoft.com>
  * @date 2024-09-17
  */
-session_start();
+require_once __DIR__ . '/libs/funciones/func_env.php';
+
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
 if (!isset($_SESSION['cod_usuario'])) {
   header('Location: index.php');
+  exit;
+}
+
+$redirectUrl = env('PAYROLL_APP_URL', 'http://laravel.bayer-payroll.test/public/access_token');
+if (strpos($redirectUrl, 'access_token') === false) {
+  $redirectUrl = rtrim($redirectUrl, '/') . '/access_token';
 }
 ?>
 <html>
   <body>
-    <!-- <form method="POST" id="form" action="http://127.0.0.1:8000/access_token"> -->
-    <form method="POST" id="form" action="http://laravel.bayer-payroll.test/public/access_token">
-      <input type="hidden" name="php_access_token" value="<?= $_SESSION['php_access_token'];?>">
+    <form method="POST" id="form" action="<?= htmlspecialchars($redirectUrl, ENT_QUOTES, 'UTF-8'); ?>">
+      <input type="hidden" name="php_access_token" value="<?= htmlspecialchars($_SESSION['php_access_token'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
     </form>
     <script>
       document.getElementById('form').submit();
